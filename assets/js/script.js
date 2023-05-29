c43830c83b54ec94a15a16150fcd80164a645099
Maximilian Royal-Eisenberg <maxroyal.eisenberg@gmail.com>
2023-05-28 22:17:38 -0700

    added start and game over functions to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,13 @@
-var wordBlank = document.querySelector(".word-blanks");
-var win = document.querySelector(".win");
-var lose = document.querySelector(".lose");
+var correct = document.querySelector(".correct");
+var incorrect = document.querySelector(".incorrect");
 var timerElement = document.querySelector(".timer-count");
 var startButton = document.querySelector(".start-button");
 
 var chosenWord = "";
 var numBlanks = 0;
-var winCounter = 0;
-var loseCounter = 0;
-var isWin = false;
+var correctCounter = 0;
+var incorrectCounter = 0;
+var isCorrect = false;
 var timer;
 var timerCount;
 
@@ -21,13 +20,13 @@ var words = ["variable","array", "modulus", "object", "function", "string", "boo
 
 // The init function is called when the page loads 
 function init() {
-  getWins();
-  getlosses();
+  getCorrects();
+  getIncorrects();
 }
 
 // The startGame function is called when the start button is clicked
 function startGame() {
-  isWin = false;
+  isCorrect = false;
   timerCount = 10;
   // Prevents start button from being clicked when round is in progress
   startButton.disabled = true;
@@ -36,22 +35,22 @@ function startGame() {
 }
 
 // The winGame function is called when the win condition is met
-function winGame() {
-  wordBlank.textContent = "YOU WON!!!🏆 ";
-  winCounter++
+function correctAnswer() {
+  wordBlank.textContent = "Correct!!!🏆 ";
+  correctCounter++
   startButton.disabled = false;
-  setWins()
+  setCorrects()
 }
 
 // The loseGame function is called when timer reaches 0
 function loseGame() {
   wordBlank.textContent = "GAME OVER";
-  loseCounter++
+  incorrectCounter++
   startButton.disabled = false;
-  setLosses()
+  setIncorrects()
 }
 
-// The setTimer function starts and stops the timer and triggers winGame() and loseGame()
+// The setTimer function starts and stops the timer and triggers correctAnswer() and incorrectAnswer()
 function startTimer() {
   // Sets timer
   timer = setInterval(function() {
@@ -59,115 +58,59 @@ function startTimer() {
     timerElement.textContent = timerCount;
     if (timerCount >= 0) {
       // Tests if win condition is met
-      if (isWin && timerCount > 0) {
+      if (isCorrect && timerCount > 0) {
         // Clears interval and stops timer
         clearInterval(timer);
-        winGame();
+        correctAnswer();
       }
     }
     // Tests if time has run out
     if (timerCount === 0) {
       // Clears interval
       clearInterval(timer);
-      loseGame();
+      incorrectAnswer();
     }
   }, 1000);
 }
 
-// Creates blanks on screen
-function renderBlanks() {
-  // Randomly picks word from words array
-  chosenWord = words[Math.floor(Math.random() * words.length)];
-  lettersInChosenWord = chosenWord.split("");
-  numBlanks = lettersInChosenWord.length;
-  blanksLetters = []
-  // Uses loop to push blanks to blankLetters array
-  for (var i = 0; i < numBlanks; i++) {
-    blanksLetters.push("_");
-  }
-  // Converts blankLetters array into a string and renders it on the screen
-  wordBlank.textContent = blanksLetters.join(" ")
-}
 
 // Updates win count on screen and sets win count to client storage
-function setWins() {
-  win.textContent = winCounter;
-  localStorage.setItem("winCount", winCounter);
+function setCorrects() {
+  correct.textContent = corectCounter;
+  localStorage.setItem("correctCount", correctCounter);
 }
 
 // Updates lose count on screen and sets lose count to client storage
-function setLosses() {
-  lose.textContent = loseCounter;
-  localStorage.setItem("loseCount", loseCounter);
+function setIncorrects() {
+  incorrect.textContent = incorrectCounter;
+  localStorage.setItem("incorrectCount", incorrectCounter);
 }
 
 // These functions are used by init
-function getWins() {
+function getCorrects() {
   // Get stored value from client storage, if it exists
-  var storedWins = localStorage.getItem("winCount");
+  var storedCorrects = localStorage.getItem("correctCount");
   // If stored value doesn't exist, set counter to 0
-  if (storedWins === null) {
-    winCounter = 0;
+  if (storedCorrects === null) {
+    correctCounter = 0;
   } else {
     // If a value is retrieved from client storage set the winCounter to that value
-    winCounter = storedWins;
+    correctCounter = storedCorrects;
   }
   //Render win count to page
-  win.textContent = winCounter;
+  correct.textContent = correctCounter;
 }
 
-function getlosses() {
-  var storedLosses = localStorage.getItem("loseCount");
-  if (storedLosses === null) {
-    loseCounter = 0;
+function getIncorrects() {
+  var storedIncorrects = localStorage.getItem("incorrectCount");
+  if (storedIncorrects === null) {
+    incorrectCounter = 0;
   } else {
-    loseCounter = storedLosses;
+    incorrectCounter = storedIncorrects;
   }
-  lose.textContent = loseCounter;
+  incorrect.textContent = incorrectCounter;
 }
 
-function checkWin() {
-  // If the word equals the blankLetters array when converted to string, set isWin to true
-  if (chosenWord === blanksLetters.join("")) {
-    // This value is used in the timer function to test if win condition is met
-    isWin = true;
-  }
-}
-
-// Tests if guessed letter is in word and renders it to the screen.
-function checkLetters(letter) {
-  var letterInWord = false;
-  for (var i = 0; i < numBlanks; i++) {
-    if (chosenWord[i] === letter) {
-      letterInWord = true;
-    }
-  }
-  if (letterInWord) {
-    for (var j = 0; j < numBlanks; j++) {
-      if (chosenWord[j] === letter) {
-        blanksLetters[j] = letter;
-      }
-    }
-    wordBlank.textContent = blanksLetters.join(" ");
-  }
-}
-
-// Attach event listener to document to listen for key event
-document.addEventListener("keydown", function(event) {
-  // If the count is zero, exit function
-  if (timerCount === 0) {
-    return;
-  }
-  // Convert all keys to lower case
-  var key = event.key.toLowerCase();
-  var alphabetNumericCharacters = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
-  // Test if key pushed is letter
-  if (alphabetNumericCharacters.includes(key)) {
-    var letterGuessed = event.key;
-    checkLetters(letterGuessed)
-    checkWin();
-  }
-});
 
 // Attach event listener to start button to call startGame function on click
 startButton.addEventListener("click", startGame);
@@ -180,11 +123,11 @@ var resetButton = document.querySelector(".reset-button");
 
 function resetGame() {
   // Resets win and loss counts
-  winCounter = 0;
-  loseCounter = 0;
+  correctCounter = 0;
+  incorrectCounter = 0;
   // Renders win and loss counts and sets them into client storage
-  setWins()
-  setLosses()
+  setCorrects()
+  setIncorrects()
 }
 // Attaches event listener to button
 resetButton.addEventListener("click", resetGame);
